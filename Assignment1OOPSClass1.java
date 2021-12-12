0cbb8247894266c627535a60d3624e9da365d3b1
ManikandanSuryaprakasam <95176608+ManikandanSuryaprakasam@users.noreply.github.com>
2021-12-12 14:27:13 +0530

    Add files via upload

---
+++ Assignment1OOPSClass1.java
@@ -0,0 +1,24 @@
+package week3.day1;
+
+public class Assignment1OOPSClass1 {
+	
+//	Assignment1:
+//		===========
+//		     Package   :org.system
+//		     Class        :Computer
+//		     Methods   :computerModel()
+//
+//		     Class        :Desktop
+//		     Methods   :desktopSize()
+//
+//		Description:
+//		create above 2 class and call all your class methods into the Desktop using single inheritance.
+	
+	
+	
+	
+	
+	
+	
+
+}
